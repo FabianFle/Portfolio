10d4d18087023130fa08356d4c1607e4de095c3b
FabianFle <fabianflegler1@yahoo.de>
2023-03-04 11:02:35 +0100

    added Projekts desin

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
 
+  projekte: any = [
+    {
+      name: 'Portfolio',
+      img: 'portfolioFrame.png',
+      programmingLanguage: 'Angular | TypeScript | HTML | CSS',
+      text: '',
+    },
+    {
+      name: 'Join',
+      img: 'JoinFrame.png',
+      programmingLanguage: 'JavaScript | HTML | CSS | Git',
+      text: '',
+    },
+    {
+      name: 'El Pollo Loco',
+      img: 'ElPolloLocoFrame.png',
+      programmingLanguage: 'JavaScript | HTML | CSS',
+      text: '',
+    },
+    {
+      name: 'Quiz-App',
+      img: 'quizFrame.png',
+      programmingLanguage: 'JavaScript | HTML | CSS',
+      text: '',
+    },
+    {
+      name: 'Pokedex',
+      img: 'pokedexFrame.png',
+      programmingLanguage: 'JavaScript | HTML | CSS | Api',
+      text: '',
+    },
+    {
+      name: 'Ring of Fire',
+      img: 'ringoffireFrame.png',
+      programmingLanguage: 'Angular | TypeScript | HTML | CSS | Firebase',
+      text: '',
+    },
+  ];
+
 }
