9b68ccc7b26ca937aefafffdf49c3664247c6fc1
FabianFle <fabianflegler1@yahoo.de>
2023-03-04 07:38:00 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StartComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    PortfolioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/skills/skills.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.scss']
+})
+export class SkillsComponent {
+
+  skills: any = [
+    {
+      name: 'Angular',
+      img: 'Angular.png'
+    },
+    {
+      name: 'TypeScript',
+      img: 'Vector (2).png'
+    },
+    {
+      name: 'JavaScript',
+      img: 'Javascript.png'
+    },
+    {
+      name: 'HTML',
+      img: 'html.png'
+    }, 
+    {
+      name: 'Firebase',
+      img: 'Firebase.png'
+    }, 
+    {
+      name: 'Git',
+      img: 'git.png'
+    },
+    {
+      name: 'CSS',
+      img: 'Vector (1).png'
+    }, 
+    {
+      name: 'Rest-Api',
+      img: 'Rest-Api.png'
+    },
+    {
+      name: 'Scrum',
+      img: 'Scrum.png'
+    },
+    {
+      name: 'Material design',
+      img: 'Frame.png'
+    }
+  ];
+
+}
