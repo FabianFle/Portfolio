beaabdd50b8cad508b68f3cdec0521c54030f0a8
FabianFle <fabianflegler1@yahoo.de>
2023-03-04 13:09:58 +0100

    finis DESKTOP opinion

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -12,37 +12,37 @@ export class PortfolioComponent {
       name: 'Portfolio',
       img: 'portfolioFrame.png',
       programmingLanguage: 'Angular | TypeScript | HTML | CSS',
-      text: '',
+      text: 'HIER MUSS ICH NOCH EINEN TEXT SCHREIBEN FÜR DIE ANWENDUNG',
     },
     {
       name: 'Join',
       img: 'JoinFrame.png',
       programmingLanguage: 'JavaScript | HTML | CSS | Git',
-      text: '',
+      text: 'HIER MUSS ICH NOCH EINEN TEXT SCHREIBEN FÜR DIE ANWENDUNG',
     },
     {
       name: 'El Pollo Loco',
       img: 'ElPolloLocoFrame.png',
       programmingLanguage: 'JavaScript | HTML | CSS',
-      text: '',
+      text: 'HIER MUSS ICH NOCH EINEN TEXT SCHREIBEN FÜR DIE ANWENDUNG',
     },
     {
       name: 'Quiz-App',
       img: 'quizFrame.png',
       programmingLanguage: 'JavaScript | HTML | CSS',
-      text: '',
+      text: 'HIER MUSS ICH NOCH EINEN TEXT SCHREIBEN FÜR DIE ANWENDUNG',
     },
     {
       name: 'Pokedex',
       img: 'pokedexFrame.png',
       programmingLanguage: 'JavaScript | HTML | CSS | Api',
-      text: '',
+      text: 'HIER MUSS ICH NOCH EINEN TEXT SCHREIBEN FÜR DIE ANWENDUNG',
     },
     {
       name: 'Ring of Fire',
       img: 'ringoffireFrame.png',
       programmingLanguage: 'Angular | TypeScript | HTML | CSS | Firebase',
-      text: '',
+      text: 'HIER MUSS ICH NOCH EINEN TEXT SCHREIBEN FÜR DIE ANWENDUNG',
     },
   ];
 
