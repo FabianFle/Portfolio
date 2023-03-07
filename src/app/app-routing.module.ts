c4381e04f40750a0b0d65d4f184b04e24c965225
FabianFle <fabianflegler1@yahoo.de>
2023-03-07 09:02:18 +0100

    responsif added, link added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { StartComponent } from './start/start.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: StartComponent},
+  {path: 'imprint', component: ImprintComponent},
+  {path: 'data-protection', component: DataProtectionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
