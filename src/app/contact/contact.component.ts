40df93ff1798629bce1c886fc7691d67433ee5a0
FabianFle <fabianflegler1@yahoo.de>
2023-03-09 10:51:23 +0100

    bug E-Mail

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  @ViewChild('myForm') myFrom!: ElementRef;
+  @ViewChild('nameField') nameField!: ElementRef;
+  @ViewChild('emailField') emailField!: ElementRef;
+  @ViewChild('messageField') messageField!: ElementRef;
+  @ViewChild('sendButton') sendButton!: ElementRef;
 
+  async sendMail() {
+    let nameField = this.nameField.nativeElement;
+    let emailField = this.emailField.nativeElement;
+    let messageField = this.messageField.nativeElement;
+    nameField.disable = true;
+    emailField.disable = true;
+    messageField.disable = true;
+
+    debugger
+    let fd = new FormData();
+    fd.append('nameField', nameField.value)
+    fd.append('emailField', emailField.value)
+    fd.append('messageField', messageField.value)
+    await fetch('https://fabianflegler.de/send_mail.php',
+      {
+        method: 'post',
+        body: fd
+      }
+    )
+  }
 }
