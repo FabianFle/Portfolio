e904dd4bed14088b58113de7fc8b20ecbbd86a89
FabianFle <fabianflegler1@yahoo.de>
2023-03-20 15:09:38 +0100

    inprint added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ImprintComponent } from './imprint/imprint.component';
-import { StartComponent } from './start/start.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
 const routes: Routes = [
-  {path: '', component: StartComponent},
+  {path: '', component: MainpageComponent},
   {path: 'imprint', component: ImprintComponent},
 ];
 
