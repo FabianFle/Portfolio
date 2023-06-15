db618187f37f9c1e59c3b71bc62ba1d85873f20d
FabianFle <fabianflegler1@yahoo.de>
2023-06-15 13:41:15 +0200

    translate bug fixed, and optimization of the site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(public router: Router) { }
+  constructor(public router: Router, public translate: TranslateService) {
+    translate.setDefaultLang('en'); // set default language
+  }
+
+  public selectedLanguage: string = 'en';
+
+
+  translateSite(language: string) {
+    this.selectedLanguage = language;
+    this.translate.use(language); // switch language
+  }
 
 }
--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imprint',
@@ -6,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./imprint.component.scss']
 })
 export class ImprintComponent implements OnInit {
+  public selectedLanguage: string = 'en';
 
-  constructor() { }
+  constructor(public translate: TranslateService) {
+    translate.setDefaultLang('en'); // set default language
+  }
+
+  translateSite(language: string) {
+    this.selectedLanguage = language;
+    this.translate.use(language); // switch language
+  }
 
   ngOnInit() { window.scrollTo(0, 0); }
 
